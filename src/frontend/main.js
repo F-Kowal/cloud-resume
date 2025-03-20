e117727c617eba8fe84e6cb64eddbab5c2f61ff9
f-kowal <fkowal.krk@gmail.com>
2025-03-20 17:02:12 +0100

    deploy azure function and frontend website to cloud storage

--- src/frontend/main.js
+++ src/frontend/main.js
@@ -2,11 +2,12 @@ window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 })
 
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApiUrl = 'https://getcounterforresume.azurewebsites.net/api/GetResumeCounter?code=pGShCeg6u57L3IQEz3ZZ4vG22iGbP2hRohLy8C3aht1YAzFuKjbemA==';
+const localFunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 0;
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response => {
         console.log("Website called function API");
